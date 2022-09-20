fd5aad0e3059ae21c8e48a849292cfc26d4fb1cc
Sankaranarayanan Shanmugasundaram <sankaranarayanan@zymr.com>
2022-09-20 16:32:01 +0530

    Added Config File for Reading Input from Environment Variables

--- frontend/src/services/EmployeeService.js
+++ frontend/src/services/EmployeeService.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import Config from '../Config';
 
-const EMPLOYEE_API_BASE_URL = process.env.BACKEND_URL;
+const EMPLOYEE_API_BASE_URL = Config.BACKEND_URL;
 
 class EmployeeService {
 
