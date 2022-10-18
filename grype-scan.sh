51103bf1f71ccec51ca3fad948996c6bb1a51e4f
kishorekumar <ykk.cse@gmail.com>
2022-10-18 16:29:20 +0530

    comments removed

--- grype-scan.sh
+++ grype-scan.sh
@@ -4,7 +4,6 @@ rm vulnerability-proj4-report-"$(date '+%Y-%m-%d')".txt
 for i in frontend:$TAG backend:$TAG mysql-db:$TAG
 do
     echo -e "+++++++++++++--------------------$i ---------------------+++++++++++++\n " >> vulnerability-proj4-report-"$(date '+%Y-%m-%d')".txt
-    #print("\n")
     grype "$i" --only-fixed >> vulnerability-proj4-report-"$(date '+%Y-%m-%d')".txt
     echo -e "\n\n" >> vulnerability-proj4-report-"$(date '+%Y-%m-%d')".txt
 done
